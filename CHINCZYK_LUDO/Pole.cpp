eb11cf39ccbac4df93cd72f5c8c1578f156ce666
Maja06063 <259707@student.pwr.edu.pl>
2021-05-20 16:29:19 +0200

    Dodawanie dokumentacji metod

--- CHINCZYK_LUDO/Pole.cpp
+++ CHINCZYK_LUDO/Pole.cpp
@@ -1,12 +1,24 @@
 #include "pch.h"
 #include "Pole.h"
 
+/// <summary>
+/// Metoda UstawWspolrzedne przypisuje danemu polu wspó³rzêdne po³o¿enia podane w parametrach.
+/// </summary>
+/// <param name="x">wspó³rzêdna pozioma</param>
+/// <param name="y">wspó³rzêdna pionowa</param>
 void Pole::ustawWspolrzedne(int x, int y)
 {
 	this->x = x;
 	this->y = y;
 }
 
+/****************************************************************************************/
+
+/// <summary>
+/// Metoda ToString s³u¿y do zwracania tekstowych informacji o danym polu.
+/// Powsta³a w celach debugowych.
+/// </summary>
+/// <returns>dane - tablica charów z tekstem zawieraj¹cym wspó³rzêdne po³o¿enia pola oraz nr pionka na tym polu</returns>
 char* Pole::ToString()
 {
 	char dane[100];
