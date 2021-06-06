e31fa8acd6bd8ae08979420def4be123a4dd9af6
Tomasz Wąsik <259728@student.pwr.edu.pl>
2021-06-06 11:25:19 +0200

    wstępne czyszczenie

--- CHINCZYK_LUDO/Gracz.cpp
+++ CHINCZYK_LUDO/Gracz.cpp
@@ -7,7 +7,7 @@
 
 void Gracz::ustawIdPolaStartowego() {
 	if (this->graczId == KolorGracza::czerwony) this->idPolaStartowego = 0;
-	if (this->graczId == KolorGracza::zielony) this->idPolaStartowego = 39;
-	if (this->graczId == KolorGracza::zolty) this->idPolaStartowego = 26;
-	if (this->graczId == KolorGracza::niebieski) this->idPolaStartowego = 13;
+	else if (this->graczId == KolorGracza::zielony) this->idPolaStartowego = 39;
+	else if (this->graczId == KolorGracza::zolty) this->idPolaStartowego = 26;
+	else if (this->graczId == KolorGracza::niebieski) this->idPolaStartowego = 13;
 }
