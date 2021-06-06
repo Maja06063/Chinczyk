ed1e2e39401d39261e0a02258c88ba0f1378c3a4
Tomasz Wąsik <259728@student.pwr.edu.pl>
2021-06-06 09:43:06 +0200

    dodanie testu jednostkowego
    jeszcze nie działa

---
+++ ChińczykTests/ChińczykTests.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../CHINCZYK_LUDO/Kostka.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ChińczykTests
+{
+	TEST_CLASS(ChińczykTests)
+	{
+	public:
+		
+		TEST_METHOD(TestKostki)
+		{
+			Kostka::Rzut();
+
+
+
+
+
+
+
+
+		}
+	};
+}
